fc449df259fb194c5b589b4ea51f4ae0938f18d8
Armin Schlegel <armin.schlegel@gmx.de>
2020-01-09 23:23:26 +0100

    Added cmake-init template

---
+++ source/mdcii/mdcii/include/mdcii/mainmenu.hpp
@@ -0,0 +1,24 @@
+#ifndef MAINMENU_H_
+#define MAINMENU_H_
+
+#include <iostream>
+#include <memory>
+#include "basegad_dat.hpp"
+#include "cod_parser.hpp"
+// #include "../thirdparty/nanogui-sdl/sdlgui/window.h"
+// #include "../thirdparty/nanogui-sdl/sdlgui/imageview.h"
+
+using namespace sdlgui;
+
+class MainMenu : public Screen
+{
+public:
+  MainMenu(const std::string& basegad_path, SDL_Window* pwindow, int rwidth, int rheight);
+  void Show();
+
+private:
+  std::string path;
+  std::shared_ptr<Cod_Parser> cod;
+  std::shared_ptr<Basegad> basegad;
+};
+#endif
