29facccd0711f07085071c390eeb0e6d81a04298
siredmar <armin.schlegel@gmx.de>
2020-05-11 15:47:28 +0200

    Add circleCi config and dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add non root user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+USER_ID=${LOCAL_USER_ID:-8000}
+
+echo "Starting with UID : ${USER_ID}"
+useradd --shell /bin/bash -u ${USER_ID} -o -c "" -m user
+export HOME=/home/user
+
+mkdir -p /build
+chown user:user /build
+
+exec gosu user "$@"% 
