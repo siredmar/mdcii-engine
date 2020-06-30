6c6f166e9405579fa47b2dbd1d8f3489cef42c67
siredmar <armin.schlegel@gmx.de>
2020-06-30 22:26:41 +0200

    Fixed chunk handling

--- source/mdcii/mdcii/src/gam/chunk.cpp
+++ source/mdcii/mdcii/src/gam/chunk.cpp
@@ -22,30 +22,41 @@
 #include "gam/chunk.hpp"
 #define CHUNK_NAME_SIZE (16u)
 
+std::vector<char> Chunk::ReadFile(const std::string& filename)
+{
+  std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
+  std::ifstream::pos_type pos = ifs.tellg();
+
+  std::vector<char> result(pos);
+
+  ifs.seekg(0, std::ios::beg);
+  ifs.read(&result[0], pos);
+  ifs.close();
+  return result;
+}
+
 std::vector<std::shared_ptr<Chunk>> Chunk::ReadChunks(const std::string& path)
 {
   std::vector<std::shared_ptr<Chunk>> chunks;
-  std::ifstream f;
-  f.open(path, std::ios_base::in | std::ios_base::binary);
-  while (!f.eof())
+  std::vector<char> bufferVector = Chunk::ReadFile(path);
+  uint32_t bufferLength = bufferVector.size();
+  char* buffer = &bufferVector[0];
+  uint32_t chunksLength = 0;
+  while (chunksLength < bufferLength)
   {
-    try
-    {
-      chunks.push_back(std::make_shared<Chunk>(f));
-    }
-    catch (const std::exception& e)
-    {
-      std::cerr << e.what() << '\n';
-    }
+    auto c = std::make_shared<Chunk>(buffer);
+    auto currentChunkLength = c->GetLength();
+    chunksLength += currentChunkLength;
+    buffer += currentChunkLength;
+    chunks.push_back(c);
   }
-  f.close();
   return chunks;
 }
 
-Chunk::Chunk(std::istream& f)
+Chunk::Chunk(const char* buffer)
 {
   char n[CHUNK_NAME_SIZE];
-  f.read(n, CHUNK_NAME_SIZE);
+  memcpy(&n, &buffer[0], CHUNK_NAME_SIZE);
   if ((strnlen(n, CHUNK_NAME_SIZE) > 0) && (strnlen(n, CHUNK_NAME_SIZE) != CHUNK_NAME_SIZE))
   {
     chunk.name = std::string(n);
@@ -54,12 +65,18 @@ Chunk::Chunk(std::istream& f)
   {
     chunk.name = "";
   }
-  f.read((char*)&chunk.length, sizeof(chunk.length));
+  memcpy(&chunk.length, &buffer[CHUNK_NAME_SIZE], sizeof(chunk.length));
   if (chunk.length > 0)
   {
     chunk.data = new uint8_t[chunk.length];
-    f.read((char*)chunk.data, chunk.length);
+    memcpy(chunk.data, &buffer[CHUNK_NAME_SIZE + sizeof(chunk.length)], chunk.length);
   }
+  chunkLength = CHUNK_NAME_SIZE + sizeof(chunk.length) + chunk.length;
+}
+
+uint32_t Chunk::GetLength()
+{
+  return chunkLength;
 }
 
 Chunk::~Chunk()
