15ba15be29390d705b86ff992dc368ec21b503f5
Armin Schlegel <armin.schlegel@gmx.de>
2020-08-03 23:29:48 +0200

    gamelist single missions working

--- source/mdcii/mdcii/include/mdcii/savegames/scenarios.hpp
+++ source/mdcii/mdcii/include/mdcii/savegames/scenarios.hpp
@@ -15,8 +15,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-#ifndef _SAVEGAMES_H_
-#define _SAVEGAMES_H_
+#ifndef _SCENARIOS_H_
+#define _SCENARIOS_H_
 
 #include <experimental/optional>
 #include <string>
@@ -27,21 +27,21 @@
 
 #include "files/files.hpp"
 
-class Savegames
+class Scenarios
 {
 public:
-    explicit Savegames(const std::string& basepath, const std::string& fileEnding);
+    explicit Scenarios(const std::string& basepath, const std::string& fileEnding);
     unsigned int size() const;
-    std::experimental::optional<std::string> GetPath(unsigned int index) const;
-    std::experimental::optional<std::string> GetName(unsigned int index) const;
-    std::experimental::optional<int> GetRanking(unsigned int index) const;
-    std::vector<std::tuple<std::string, std::string, int>> GetSavegames() const;
-    // GamesPb::Games Get() const;
+    // std::experimental::optional<std::string> GetPath(unsigned int index) const;
+    // std::experimental::optional<std::string> GetName(unsigned int index) const;
+    // std::experimental::optional<int> GetRanking(unsigned int index) const;
+    GamesPb::Games Get() const;
+    // std::vector<std::tuple<std::string, std::string, int>> Get() const;
 
 private:
     // vector element contains: path, name, ranking
-    // GamesPb::Games gamelist;
-    std::vector<std::tuple<std::string, std::string, int>> savegames;
+    GamesPb::Games gamelist;
+    // std::vector<std::tuple<std::string, std::string, int>> savegames;
 };
 
 #endif // _SAVEGAMES_H_
--- source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/texturetable.h
+++ source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/texturetable.h
@@ -19,6 +19,119 @@
 
 NAMESPACE_BEGIN(sdlgui)
 
+class TextureTableBase
+{
+public:
+    // void setVisibleElements(int visible)
+    // {
+    //     visibleElements = visible;
+    // }
+
+    // void setScrollBy(int scroll)
+    // {
+    //     scrollBy = scroll;
+    // }
+
+    // void setMouseScroll(bool state)
+    // {
+    //     mouseScroll = state;
+    // }
+
+    // void setVerticalMargin(int margin)
+    // {
+    //     verticalMargin = margin;
+    // }
+
+    // bool scrollEvent(const Vector2i& p, const Vector2f& rel)
+    // {
+    //     if (mouseScroll)
+    //     {
+    //         if (rel.y < 0)
+    //         {
+    //             scrollNegative();
+    //             setVisibleFlag = false;
+    //             return true;
+    //         }
+    //         else if (rel.y > 0)
+    //         {
+    //             scrollPositive();
+    //             setVisibleFlag = false;
+    //             return true;
+    //         }
+    //     }
+    //     return Widget::scrollEvent(p, rel);
+    // }
+
+    // void setPosition(const Vector2i& pos)
+    // {
+    //     _pos = pos;
+    // }
+
+    // void setPosition(int x, int y)
+    // {
+    //     _pos = Vector2i{ x, y };
+    // }
+
+    // Vector2i preferredSize(SDL_Renderer* ctx) const
+    // {
+    //     return size;
+    // }
+
+    // virtual void draw(SDL_Renderer* renderer)
+    // {
+    //     // ...
+    // }
+
+    // bool mouseMotionEvent(const Vector2i& p, const Vector2i& rel, int button, int modifiers)
+    // {
+    //     // ...
+    // }
+
+    // virtual uint32_t elementsCount()
+    // {
+    //     // ...
+    // }
+
+    virtual void scrollPositive()
+    {
+        // ...
+    }
+
+    virtual void scrollNegative()
+    {
+        // ...
+    }
+
+    virtual Widget* childAt(int index)
+    {
+        // ...
+    }
+
+    // virtual bool removeElement(unsigned int index)
+    // {
+    //     // ...
+    // }
+
+    // virtual void clear()
+    // {
+    //     // ...
+    // }
+
+protected:
+    // Vector2i size;
+    // unsigned int visibleElements;
+    // int scrollBy;
+    // int verticalMargin;
+    // bool mouseScroll;
+    // int currentScrollIndex;
+    // int visibleFrom;
+    // int visibleTo;
+    // int visibleFromOld;
+    // int visibleToOld;
+    // Widget* tableaddr;
+    // bool setVisibleFlag = false;
+};
+
 /**
  * \class TextureTable texturetable.h sdl_gui/texturetable.h
  *
@@ -26,51 +139,215 @@ NAMESPACE_BEGIN(sdlgui)
  *
  * Table view with TextureButtons
  */
-class TextureTable : public Widget
+template <typename T>
+class TextureTable : public TextureTableBase, public Widget
 {
 public:
-  TextureTable(Widget* parent, Vector2i pos, Vector2i size, std::function<Widget*(const std::vector<std::tuple<std::string, std::string, int>>&)> factory,
-      const std::vector<std::tuple<std::string, std::string, int>>& data, unsigned int visibleElements = 10, int scrollBy = 3, int verticalMargin = 2,
-      bool mouseScroll = false);
+    TextureTable(Widget* parent, Vector2i pos, Vector2i size, std::function<Widget*(T)> factory,
+        T data, unsigned int visibleElements = 10, int scrollBy = 3, int verticalMargin = 2,
+        bool mouseScroll = false)
+        : Widget(parent)
+        , pos(pos)
+        , size(size)
+        , visibleElements(visibleElements)
+        , scrollBy(scrollBy)
+        , verticalMargin(verticalMargin)
+        , mouseScroll(mouseScroll)
+        , visibleFrom(0)
+        , visibleTo(visibleFrom + visibleElements)
+        , visibleFromOld(visibleFrom)
+        , visibleToOld(visibleTo)
+        , tableaddr(factory(data))
+    {
+        tableaddr->setSize(size);
+        addChild(tableaddr);
+        if (tableaddr->childCount() < visibleElements)
+        {
+            visibleTo = tableaddr->childCount();
+            visibleToOld = visibleTo;
+        }
+    }
 
-  /// Compute the size needed to fully display the texture view
-  virtual Vector2i preferredSize(SDL_Renderer* ctx) const override;
-  void setVisibleElements(int visible);
-  void setScrollBy(int scroll);
-  void setVerticalMargin(int margin);
-  /// Set the position relative to the parent widget
-  void setPosition(const Vector2i& pos);
-  void setPosition(int x, int y);
-  void setVisible(bool visible);
-  void setMouseScroll(bool state);
+    Vector2i preferredSize(SDL_Renderer* ctx) const
+    {
+        return size;
+    }
 
-  bool mouseMotionEvent(const Vector2i& p, const Vector2i& rel, int button, int modifiers);
-  bool scrollEvent(const Vector2i& p, const Vector2f& rel);
-  /// Draw the texture view
-  void draw(SDL_Renderer* renderer) override;
-  uint32_t elementsCount();
-  void scrollNegative();
-  void scrollPositive();
-  void addElement(Widget widget);
-  void addElements(std::vector<Widget> widget);
-  void updateElements(std::vector<Widget> widget);
-  bool removeElement(unsigned int index);
-  void clear();
+    bool mouseMotionEvent(const Vector2i& p, const Vector2i& rel, int button, int modifiers)
+    {
+        for (auto i = 0; i < tableaddr->childCount(); i++)
+        {
+            auto child = tableaddr->childAt(i);
+            if (!child->visible())
+                continue;
+            bool contained = child->contains(p - _pos);
+            bool prevContained = child->contains(p - _pos - rel);
+            if (contained != prevContained)
+                child->mouseEnterEvent(p, contained);
+            if ((contained || prevContained) && child->mouseMotionEvent(p - _pos, rel, button, modifiers))
+                return true;
+        }
+        return false;
+    }
+
+    void draw(SDL_Renderer* renderer)
+    {
+        Widget::draw(renderer);
+        int y = verticalMargin;
+        setVisibleFlag = false;
+        if (setVisibleFlag == false)
+        {
+            for (int i = visibleFromOld; i < visibleToOld; i++)
+            {
+                tableaddr->childAt(i)->setVisible(false);
+            }
+            visibleFromOld = visibleFrom;
+            visibleToOld = visibleTo;
+            for (int i = visibleFrom; i < visibleTo; i++)
+            {
+                Widget* w = tableaddr->childAt(i);
+                w->setPosition(0, y + verticalMargin);
+                w->setVisible(true);
+                if (setVisibleFlag == false)
+                {
+                    setVisibleFlag = true;
+                }
+                y += w->childAt(0)->size()[1];
+            }
+        }
+    }
+
+    uint32_t elementsCount()
+    {
+        return tableaddr->childCount();
+    }
+
+    void scrollPositive()
+    {
+        if (visibleFrom - scrollBy <= 0)
+        {
+            visibleFrom = 0;
+        }
+        else
+        {
+            visibleFrom -= scrollBy;
+        }
+        int visibleRange = visibleElements;
+        if (tableaddr->childCount() < visibleElements)
+        {
+            visibleRange = tableaddr->childCount();
+        }
+        visibleTo = visibleFrom + visibleRange;
+    }
+
+    void scrollNegative()
+    {
+        if (visibleTo + scrollBy > elementsCount())
+        {
+            visibleTo = elementsCount();
+        }
+        else
+        {
+            visibleTo += scrollBy;
+        }
+        int visibleRange = visibleElements;
+        if (tableaddr->childCount() < visibleElements)
+        {
+            visibleRange = tableaddr->childCount();
+        }
+        visibleFrom = visibleTo - visibleRange;
+    }
+
+    bool removeElement(unsigned int index)
+    {
+
+        if (index >= tableaddr->childCount())
+        {
+            return false;
+        }
+        tableaddr->removeChild(index);
+        return true;
+    }
+
+    void clear()
+    {
+        for (int i = 0; i < tableaddr->childCount(); i++)
+        {
+            tableaddr->removeChild(i);
+        }
+    }
+
+    void setVisible(bool visible)
+    {
+        mVisible = visible;
+        tableaddr->setVisible(visible);
+    }
+
+    ////
+    void setVisibleElements(int visible)
+    {
+        visibleElements = visible;
+    }
+
+    void setScrollBy(int scroll)
+    {
+        scrollBy = scroll;
+    }
+
+    void setMouseScroll(bool state)
+    {
+        mouseScroll = state;
+    }
+
+    void setVerticalMargin(int margin)
+    {
+        verticalMargin = margin;
+    }
+
+    bool scrollEvent(const Vector2i& p, const Vector2f& rel)
+    {
+        if (mouseScroll)
+        {
+            if (rel.y < 0)
+            {
+                scrollNegative();
+                setVisibleFlag = false;
+                return true;
+            }
+            else if (rel.y > 0)
+            {
+                scrollPositive();
+                setVisibleFlag = false;
+                return true;
+            }
+        }
+        return Widget::scrollEvent(p, rel);
+    }
+
+    void setPosition(const Vector2i& pos)
+    {
+        _pos = pos;
+    }
+
+    void setPosition(int x, int y)
+    {
+        _pos = Vector2i{ x, y };
+    }
 
 protected:
-  Vector2i pos;
-  Vector2i size;
-  unsigned int visibleElements;
-  int scrollBy;
-  int verticalMargin;
-  bool mouseScroll;
-  int currentScrollIndex;
-  int visibleFrom;
-  int visibleTo;
-  int visibleFromOld;
-  int visibleToOld;
-  Widget* tableaddr;
-  bool setVisibleFlag = false;
+    Vector2i pos;
+    Vector2i size;
+    unsigned int visibleElements;
+    int scrollBy;
+    int verticalMargin;
+    bool mouseScroll;
+    int currentScrollIndex;
+    int visibleFrom;
+    int visibleTo;
+    int visibleFromOld;
+    int visibleToOld;
+    Widget* tableaddr;
+    bool setVisibleFlag = false;
 };
 
 NAMESPACE_END(sdlgui)
