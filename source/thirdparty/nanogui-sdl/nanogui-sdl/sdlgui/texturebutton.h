0808aba495eab2c8bf50e587ed009bc5029d9358
Armin Schlegel <armin.schlegel@gmx.de>
2020-08-08 17:08:07 +0200

    textureButton nur supporting toggle button

--- source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/texturebutton.h
+++ source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/texturebutton.h
@@ -26,78 +26,119 @@ NAMESPACE_BEGIN(sdlgui)
 class TextureButton : public Widget
 {
 public:
-  enum Flags
-  {
-    OnClick = (1 << 0), // 1
-    OnHover = (1 << 1), // 2
-  };
+    enum Flags
+    {
+        NormalButton = (1 << 0), // 1
+        ToggleButton = (1 << 1), // 2
+        OnClick = (1 << 2), // 4
+        OnHover = (1 << 3) // 8
+    };
 
-  TextureButton(Widget* parent, SDL_Texture* primaryTexture);
-  TextureButton(Widget* parent, SDL_Texture* primaryTexture, const std::function<void()>& callback)
-    : TextureButton(parent, primaryTexture)
-  {
-    setCallback(callback);
-  }
-  TextureButton(Widget* parent, SDL_Texture* primaryTexture, SDL_Texture* secondaryTexture, const std::function<void()>& callback)
-    : TextureButton(parent, primaryTexture)
-  {
-    setSecondaryTexture(secondaryTexture);
-    setCallback(callback);
-  }
+    TextureButton(Widget* parent, SDL_Texture* primaryTexture);
+    TextureButton(Widget* parent, SDL_Texture* primaryTexture, const std::function<void()>& callback)
+        : TextureButton(parent, primaryTexture)
+    {
+        setCallback(callback);
+    }
+    TextureButton(Widget* parent, SDL_Texture* primaryTexture, SDL_Texture* secondaryTexture, const std::function<void()>& callback)
+        : TextureButton(parent, primaryTexture)
+    {
+        setSecondaryTexture(secondaryTexture);
+        setCallback(callback);
+    }
 
-  bool pushed() const { return mPushed; }
-  void setPushed(bool pushed) { mPushed = pushed; }
+    bool pushed() const
+    {
+        return mPushed;
+    }
+    void setPushed(bool pushed)
+    {
+        mPushed = pushed;
+        if (pushed)
+        {
+            mTexture = mSecondaryTexture;
+        }
+        else
+        {
+            mTexture = mPrimaryTexture;
+        }
+    }
 
-  /// Set the push callback (for any type of button)
-  std::function<void()> callback() const { return mCallback; }
-  void setCallback(const std::function<void()>& callback) { mCallback = callback; }
+    /// Set the push callback (for any type of button)
+    std::function<void()> callback() const
+    {
+        return mCallback;
+    }
+    void setCallback(const std::function<void()>& callback)
+    {
+        mCallback = callback;
+    }
 
-  int textureSwitchFlags() const { return mFlags; }
-  void setTextureSwitchFlags(int buttonFlags) { mFlags = buttonFlags; }
+    int flags() const
+    {
+        return mFlags;
+    }
 
-  /// Get the primary SDL_Texture
-  SDL_Texture* primaryTexture() const { return mPrimaryTexture; }
-  /// Set the primary SDL_Texture
-  void setPrimaryTexture(SDL_Texture* texture) { mPrimaryTexture = texture; }
+    void setFlags(int buttonFlags)
+    {
+        mFlags = buttonFlags;
+    }
 
-  /// Get the secondary SDL_Texture
-  SDL_Texture* secondaryTexture() const { return mSecondaryTexture; }
-  /// Set the secondary SDL_Texture
-  void setSecondaryTexture(SDL_Texture* texture) { mSecondaryTexture = texture; }
+    /// Get the primary SDL_Texture
+    SDL_Texture* primaryTexture() const
+    {
+        return mPrimaryTexture;
+    }
+    /// Set the primary SDL_Texture
+    void setPrimaryTexture(SDL_Texture* texture)
+    {
+        mPrimaryTexture = texture;
+    }
 
-  /// Compute the size needed to fully display the texture button
-  virtual Vector2i preferredSize(SDL_Renderer* ctx) const override;
-  virtual bool mouseButtonEvent(const Vector2i& p, int button, bool down, int modifiers) override;
-  virtual bool mouseEnterEvent(const Vector2i& p, bool enter) override;
-  /// Draw the texture button
-  void draw(SDL_Renderer* renderer) override;
+    /// Get the secondary SDL_Texture
+    SDL_Texture* secondaryTexture() const
+    {
+        return mSecondaryTexture;
+    }
+    /// Set the secondary SDL_Texture
+    void setSecondaryTexture(SDL_Texture* texture)
+    {
+        mSecondaryTexture = texture;
+    }
 
-  TextureButton& withCallback(const std::function<void()>& callback)
-  {
-    setCallback(callback);
-    return *this;
-  }
+    /// Compute the size needed to fully display the texture button
+    virtual Vector2i preferredSize(SDL_Renderer* ctx) const override;
+    virtual bool mouseButtonEvent(const Vector2i& p, int button, bool down, int modifiers) override;
+    virtual bool mouseEnterEvent(const Vector2i& p, bool enter) override;
+    /// Draw the texture button
+    void draw(SDL_Renderer* renderer) override;
 
-  TextureButton& withSecondaryTexture(SDL_Texture* texture)
-  {
-    setSecondaryTexture(texture);
-    return *this;
-  }
+    TextureButton& withCallback(const std::function<void()>& callback)
+    {
+        setCallback(callback);
+        return *this;
+    }
 
-  TextureButton& withTextureSwitchFlags(int flags)
-  {
-    setTextureSwitchFlags(flags);
-    return *this;
-  }
+    TextureButton& withSecondaryTexture(SDL_Texture* texture)
+    {
+        setSecondaryTexture(texture);
+        return *this;
+    }
+
+    TextureButton& withTextureSwitchFlags(int flags)
+    {
+        setFlags(flags);
+        return *this;
+    }
 
 protected:
-  SDL_Texture* mPrimaryTexture;
-  SDL_Texture* mTexture;
-  bool mPushed;
-  int mFlags;
-  bool _pushedTemp;
-  SDL_Texture* mSecondaryTexture;
-  std::function<void()> mCallback;
+    SDL_Texture* mPrimaryTexture;
+    SDL_Texture* mTexture;
+    bool mPushed;
+    int mFlags;
+    bool _pushedTemp;
+    SDL_Texture* mSecondaryTexture;
+    std::function<void()> mCallback;
 };
 
 NAMESPACE_END(sdlgui)
