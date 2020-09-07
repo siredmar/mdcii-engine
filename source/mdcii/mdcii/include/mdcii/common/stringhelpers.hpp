e8e3c4022b0de31854e9e801e137383e493b2310
Armin Schlegel <armin.schlegel@gmx.de>
2020-09-07 07:37:03 +0200

    updated label in nanogui-sdl

--- source/mdcii/mdcii/include/mdcii/common/stringhelpers.hpp
+++ source/mdcii/mdcii/include/mdcii/common/stringhelpers.hpp
@@ -3,5 +3,6 @@
 
 std::wstring stringToWstring(const std::string& utf8Str);
 std::string iso_8859_1_to_utf8(std::string& str);
+std::string removeTrailingCarriageReturnNewline(const std::string& input);
 
 #endif // __STRINGHELPERS_HPP_
--- source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/label.cpp
+++ source/thirdparty/nanogui-sdl/nanogui-sdl/sdlgui/label.cpp
@@ -12,6 +12,12 @@
 
 #include <SDL2/SDL.h>
 
+#if defined(_WIN32)
+#include <SDL_image.h>
+#else
+#include <SDL2/SDL_image.h>
+#endif
+
 #include <sdlgui/label.h>
 #include <sdlgui/theme.h>
 #include <sdlgui/vscrollpanel.h>
@@ -57,20 +63,20 @@ Vector2i Label::preferredSize(SDL_Renderer* ctx) const
         return Vector2i::Zero();
     }
 
-    if (mFixedSize.x > 0)
-    {
-        int w, h;
-        // const_cast<Label*>(this)->mTheme->getUtf8Bounds(mFont.c_str(), fontSize(), mCaption.c_str(), &w, &h);
-        SDL_QueryTexture(_texture.tex, nullptr, nullptr, &w, &h);
-        return Vector2i(mFixedSize.x, h);
-    }
-    else
-    {
-        int w, h;
-        // const_cast<Label*>(this)->mTheme->getUtf8Bounds(mFont.c_str(), fontSize(), mCaption.c_str(), &w, &h);
-        SDL_QueryTexture(_texture.tex, nullptr, nullptr, &w, &h);
-        return Vector2i(w, h);
-    }
+    // if (mFixedSize.x > 0)
+    // {
+    //     int w, h;
+    //     // const_cast<Label*>(this)->mTheme->getUtf8Bounds(mFont.c_str(), fontSize(), mCaption.c_str(), &w, &h);
+    //     SDL_QueryTexture(_texture.tex, nullptr, nullptr, &w, &h);
+    //     return Vector2i(mFixedSize.x, h);
+    // }
+    // else
+    // {
+    int w, h;
+    // const_cast<Label*>(this)->mTheme->getUtf8Bounds(mFont.c_str(), fontSize(), mCaption.c_str(), &w, &h);
+    SDL_QueryTexture(_texture.tex, nullptr, nullptr, &w, &h);
+    return Vector2i(w, h);
+    // }
 }
 
 void Label::setFontSize(int fontSize)
@@ -79,8 +85,22 @@ void Label::setFontSize(int fontSize)
     _texture.dirty = true;
 }
 
+// void Label::save_texture(const char* file_name, SDL_Renderer* renderer, SDL_Texture* texture)
+// {
+//     SDL_Texture* target = SDL_GetRenderTarget(renderer);
+//     SDL_SetRenderTarget(renderer, texture);
+//     int width, height;
+//     SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+//     SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+//     SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch);
+//     IMG_SavePNG(surface, file_name);
+//     SDL_FreeSurface(surface);
+//     SDL_SetRenderTarget(renderer, target);
+// }
+
 void Label::draw(SDL_Renderer* renderer)
 {
+    static bool bmp = false;
     Widget::draw(renderer);
 
     // if (mParent->getType() == "VScrollPanel")
@@ -93,17 +113,32 @@ void Label::draw(SDL_Renderer* renderer)
         mTheme->getTexAndRectUtf8(renderer, _texture, 0, 0, mCaption.c_str(), mFont.c_str(), fontSize(), mColor, mMultiline);
         if (mMultiline != -1)
         {
-            setSize(Vector2i{ _texture.w(), _texture.h() });
+            // if (bmp == false)
+            // {
+            //     save_texture("text.png", renderer, _texture.tex);
+            // }
+            // bmp = true;
+
+            setSize(preferredSize(nullptr));
         }
     }
 
     if (mParent->getType() == "VScrollPanel" && mParent->size().y < mSize.y)
     {
         auto scrollParent = dynamic_cast<VScrollPanel*>(mParent);
-        float maxScroll = scrollParent->scroll() * float(scrollParent->size().y - 20) / float(mSize.y);
+        float maxScroll = scrollParent->scroll(); // * float(scrollParent->size().y) / float(mSize.y);
+        // float maxScroll = scrollParent->scroll() * float(scrollParent->size().y - 20) / float(mSize.y);
         std::cout << "msize.y: " << mSize.y << ", parent.y: " << scrollParent->size().y << " maxScroll: " << maxScroll << " " << scrollParent->scroll() << std::endl;
         // SDL_Rect srcrect{ 0, (maxScroll * scrollParent->size().y) - (mSize.y - _texture.rrect.h) * 0.5f, scrollParent->size().x, scrollParent->size().y };
-        SDL_Rect srcrect{ 0, (maxScroll * scrollParent->size().y), scrollParent->size().x, scrollParent->size().y };
+        int y = (maxScroll * scrollParent->size().y);
+        std::cout << "src: x: " << 0 << ", y: " << y << ", w: " << mSize.x << ", h: " << scrollParent->size().y << std::endl;
+        int y_end = y + scrollParent->size().y;
+        std::cout << "y_end: " << y_end << std::endl;
+        if (y_end > mSize.y)
+        {
+            y = mSize.y;
+        }
+        SDL_Rect srcrect{ 0, y, mSize.x, scrollParent->size().y };
         SDL_Rect destrect{ scrollParent->absolutePosition().x, scrollParent->absolutePosition().y, _texture.w(), scrollParent->size().y };
         SDL_RenderCopyOriginal(renderer, _texture.tex, &srcrect, &destrect);
     }
