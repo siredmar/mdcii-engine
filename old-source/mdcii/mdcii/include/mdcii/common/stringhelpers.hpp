0135b83a6adfd6ac0e54ebfcea457517e48f54a3
Armin Schlegel <armin.schlegel@gmx.de>
2024-01-08 15:18:41 +0100

    fix: got rid of old C++ source in the project tree

--- old-source/mdcii/mdcii/include/mdcii/common/stringhelpers.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __STRINGHELPERS_HPP_
-#define __STRINGHELPERS_HPP_
-
-std::wstring stringToWstring(const std::string& utf8Str);
-std::string iso_8859_1_to_utf8(std::string& str);
-std::string removeTrailingCarriageReturnNewline(const std::string& input);
-
-#endif // __STRINGHELPERS_HPP_
