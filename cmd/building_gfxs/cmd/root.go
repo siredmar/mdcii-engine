f441acb9d5ba9fe29719151b193c6dd915411e1d
Armin Schlegel <armin.schlegel@gmx.de>
2024-12-02 10:19:01 +0100

    feat: building package

--- cmd/building_gfxs/cmd/root.go
+++ cmd/building_gfxs/cmd/root.go
@@ -26,6 +26,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/siredmar/mdcii-engine/pkg/bsh"
+	building "github.com/siredmar/mdcii-engine/pkg/building"
 	"github.com/siredmar/mdcii-engine/pkg/cod"
 	"github.com/siredmar/mdcii-engine/pkg/cod/buildings"
 	buildingsCod "github.com/siredmar/mdcii-engine/pkg/cod/buildings"
@@ -132,30 +133,18 @@ func Execute() {
 }
 
 func (g *Game) setBuilding(id int) {
-	building := g.buildings.BuildingsVector[id]
-	g.Building = &Building{
-		BaseIndexSaved:       building.Gfx,
-		BaseIndex:            building.Gfx,
+	b := g.buildings.BuildingsVector[id]
+	g.Building = &building.Building{
+		BaseIndexSaved:       b.Gfx,
+		BaseIndex:            b.Gfx,
 		Rotation:             0,
 		X:                    ScreenWidth / 4,
 		Y:                    ScreenHeight / 4,
-		AnimationSteps:       building.AnimationAmount,
+		AnimationSteps:       b.AnimationAmount,
 		CurrentAnimationStep: 0,
-		AnimationAdd:         building.AnimationAdd,
-		Size:                 buildingSize(building.Size.W, building.Size.H),
+		AnimationAdd:         b.AnimationAdd,
+		Size:                 building.BuildingSize(b.Size.W, b.Size.H),
 	}
-}
-
-// Building enthält die Informationen eines Gebäudes
-type Building struct {
-	BaseIndexSaved       int
-	BaseIndex            int
-	Rotation             int
-	X, Y                 int
-	AnimationSteps       int
-	CurrentAnimationStep int
-	AnimationAdd         int
-	Size                 BuildingSizeIdentifier
 }
 
 type Game struct {
@@ -169,185 +158,14 @@ type Game struct {
 	buffer           *ebiten.Image
 	op               *ebiten.DrawImageOptions
 	drawToBuffer     bool
-	Building         *Building
+	Building         *building.Building
 	lastKeyPressTime time.Time
 	buildingId       int
 }
 
-type BuildingSizeIdentifier int
-
-const (
-	BuildingSize1x1 BuildingSizeIdentifier = iota
-	BuildingSize1x2
-	BuildingSize1x3
-	BuildingSize2x2
-	BuildingSize2x1
-	BuildingSize2x3
-	BuildingSize3x3
-	BuildingSize4x4
-	BuildingSize4x3
-	BuildingSize5x5
-	BuildingSize6x6
-	BuildingSize6x4
-	BuildingSize5x7
-	BuildingSizeUnknown
-)
-
-func buildingSize(w, h int) BuildingSizeIdentifier {
-	switch {
-	case w == 1 && h == 1:
-		return BuildingSize1x1
-	case w == 1 && h == 2:
-		return BuildingSize1x2
-	case w == 1 && h == 3:
-		return BuildingSize1x3
-	case w == 2 && h == 1:
-		return BuildingSize2x1
-	case w == 2 && h == 2:
-		return BuildingSize2x2
-	case w == 2 && h == 3:
-		return BuildingSize2x3
-	case w == 3 && h == 3:
-		return BuildingSize3x3
-	case w == 4 && h == 3:
-		return BuildingSize4x3
-	case w == 4 && h == 4:
-		return BuildingSize4x4
-	case w == 5 && h == 5:
-		return BuildingSize5x5
-	case w == 6 && h == 6:
-		return BuildingSize6x6
-	case w == 6 && h == 4:
-		return BuildingSize6x4
-	case w == 5 && h == 7:
-		return BuildingSize5x7
-	}
-
-	return BuildingSizeUnknown
-}
-
-var (
-	rotationOffsets = map[BuildingSizeIdentifier][][][]int{
-		BuildingSize1x1: {
-			{{0, 0}}, // Rotation 0
-			{{0, 0}}, // Rotation 1
-			{{0, 0}}, // Rotation 2
-			{{0, 0}}, // Rotation 3
-		},
-		BuildingSize2x2: {
-			{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, // Rotation 0
-			{{1, 0}, {1, 1}, {0, 0}, {0, 1}}, // Rotation 1
-			{{1, 1}, {0, 1}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 1}, {0, 0}, {1, 1}, {1, 0}}, // Rotation 3
-		},
-		BuildingSize2x3: {
-			{{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}}, // Rotation 0
-			{{2, 0}, {2, 1}, {1, 0}, {1, 1}, {0, 0}, {0, 1}}, // Rotation 1
-			{{1, 2}, {0, 2}, {1, 1}, {0, 1}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 1}, {0, 0}, {1, 1}, {1, 0}, {2, 1}, {2, 0}}, // Rotation 3
-		},
-		BuildingSize2x1: {
-			{{0, 0}, {1, 0}}, // Rotation 0: horizontal (2 wide, 1 tall)
-			{{1, 0}, {1, 1}}, // Rotation 1: vertical (1 wide, 2 tall)
-			{{1, 1}, {0, 1}}, // Rotation 2: horizontal flipped (2 wide, 1 tall)
-			{{0, 1}, {0, 0}}, // Rotation 3: vertical flipped (1 wide, 2 tall)
-		},
-		BuildingSize1x2: {
-			{{0, 0}, {0, 1}}, // Rotation 0
-			{{1, 1}, {0, 1}}, // Rotation 1
-			{{0, 1}, {0, 0}}, // Rotation 2
-			{{0, 1}, {1, 1}}, // Rotation 3
-		},
-		BuildingSize1x3: {
-			{{0, 0}, {0, 1}, {0, 2}}, // Rotation 0
-			{{2, 0}, {1, 0}, {0, 0}}, // Rotation 1
-			{{0, 2}, {0, 1}, {0, 0}}, // Rotation 2
-			{{0, 0}, {1, 0}, {2, 0}}, // Rotation 3
-		},
-		BuildingSize3x3: {
-			{{0, 0}, {1, 0}, {2, 0}, {0, 1}, {1, 1}, {2, 1}, {0, 2}, {1, 2}, {2, 2}}, // Rotation 0
-			{{2, 0}, {2, 1}, {2, 2}, {1, 0}, {1, 1}, {1, 2}, {0, 0}, {0, 1}, {0, 2}}, // Rotation 1
-			{{2, 2}, {1, 2}, {0, 2}, {2, 1}, {1, 1}, {0, 1}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 2}, {0, 1}, {0, 0}, {1, 2}, {1, 1}, {1, 0}, {2, 2}, {2, 1}, {2, 0}}, // Rotation 3
-		},
-		BuildingSize4x3: {
-			{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}}, // Rotation 0: 4 wide, 3 tall
-			{{2, 0}, {2, 1}, {2, 2}, {2, 3}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {0, 0}, {0, 1}, {0, 2}, {0, 3}}, // Rotation 1: 3 wide, 4 tall (rotated 90° clockwise)
-			{{3, 2}, {2, 2}, {1, 2}, {0, 2}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {3, 0}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2: 4 wide, 3 tall (rotated 180°)
-			{{0, 3}, {0, 2}, {0, 1}, {0, 0}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {2, 3}, {2, 2}, {2, 1}, {2, 0}}, // Rotation 3: 3 wide, 4 tall (rotated 270° clockwise)
-		},
-		BuildingSize4x4: {
-			{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {0, 3}, {1, 3}, {2, 3}, {3, 3}}, // Rotation 0
-			{{3, 0}, {3, 1}, {3, 2}, {3, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {0, 0}, {0, 1}, {0, 2}, {0, 3}}, // Rotation 1
-			{{3, 3}, {2, 3}, {1, 3}, {0, 3}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {3, 0}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 3}, {0, 2}, {0, 1}, {0, 0}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {3, 3}, {3, 2}, {3, 1}, {3, 0}}, // Rotation 3
-		},
-		BuildingSize5x5: {
-			{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}}, // Rotation 0
-			{{4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}}, // Rotation 1
-			{{4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {4, 0}}, // Rotation 3
-		},
-		BuildingSize6x6: {
-			{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}}, // Rotation 0
-			{{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}, {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}}, // Rotation 1
-			{{5, 5}, {4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}, {5, 4}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, {5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, {5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}, {1, 5}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {2, 5}, {2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {3, 5}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {4, 5}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {4, 0}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {5, 1}, {5, 0}}, // Rotation 3
-		},
-		BuildingSize6x4: {
-			{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}}, // Rotation 0
-			{{3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}}, // Rotation 1
-			{{5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, {5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}}, // Rotation 2
-			{{0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}, {1, 5}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {2, 5}, {2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {3, 5}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}}, // Rotation 3
-		},
-		BuildingSize5x7: {
-			// Rotation 0: 5 wide, 7 tall
-			{
-				{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0},
-				{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1},
-				{0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2},
-				{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3},
-				{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4},
-				{0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5},
-				{0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6},
-			},
-			// Rotation 1: 7 wide, 5 tall (rotated 90° clockwise)
-			{
-				{6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4},
-				{5, 0}, {5, 1}, {5, 2}, {5, 3}, {5, 4},
-				{4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4},
-				{3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4},
-				{2, 0}, {2, 1}, {2, 2}, {2, 3}, {2, 4},
-				{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4},
-				{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4},
-			},
-			// Rotation 2: 5 wide, 7 tall (rotated 180°)
-			{
-				{4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6},
-				{4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5},
-				{4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4},
-				{4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3},
-				{4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2},
-				{4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1},
-				{4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0},
-			},
-			// Rotation 3: 7 wide, 5 tall (rotated 270° clockwise)
-			{
-				{0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0},
-				{1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0},
-				{2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0},
-				{3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0},
-				{4, 4}, {4, 3}, {4, 2}, {4, 1}, {4, 0},
-				{5, 4}, {5, 3}, {5, 2}, {5, 1}, {5, 0},
-				{6, 4}, {6, 3}, {6, 2}, {6, 1}, {6, 0},
-			},
-		},
-	}
-)
-
 func (g *Game) Draw(screen *ebiten.Image) {
 	if g.Building != nil {
-		offsets := rotationOffsets[g.Building.Size][g.Building.Rotation]
+		offsets := building.RotationOffsets[g.Building.Size][g.Building.Rotation]
 		for i, offset := range offsets {
 			screenX := float64(g.Building.X) + float64(offset[0]-offset[1])*(float64(tileWidth)/2)
 			screenY := float64(g.Building.Y) + float64(offset[0]+offset[1])*(float64(tileHeight)/2)
@@ -380,8 +198,8 @@ func calculateBaseOffsetY(tileHeight, gridTileHeight int) int {
 	return 0
 }
 
-func calculateTextureKey(baseIndex, rotation, tileIndex int, size BuildingSizeIdentifier) string {
-	tilesPerRotation := len(rotationOffsets[size][rotation])
+func calculateTextureKey(baseIndex, rotation, tileIndex int, size building.BuildingSizeIdentifier) string {
+	tilesPerRotation := len(building.RotationOffsets[size][rotation])
 	return fmt.Sprintf("%d", baseIndex+(rotation*tilesPerRotation)+tileIndex)
 }
 
---
+++ pkg/building/building.go
@@ -0,0 +1,65 @@
+package building
+
+type BuildingSizeIdentifier int
+
+const (
+	BuildingSize1x1 BuildingSizeIdentifier = iota
+	BuildingSize1x2
+	BuildingSize1x3
+	BuildingSize2x2
+	BuildingSize2x1
+	BuildingSize2x3
+	BuildingSize3x3
+	BuildingSize4x4
+	BuildingSize4x3
+	BuildingSize5x5
+	BuildingSize6x6
+	BuildingSize6x4
+	BuildingSize5x7
+	BuildingSizeUnknown
+)
+
+func BuildingSize(w, h int) BuildingSizeIdentifier {
+	switch {
+	case w == 1 && h == 1:
+		return BuildingSize1x1
+	case w == 1 && h == 2:
+		return BuildingSize1x2
+	case w == 1 && h == 3:
+		return BuildingSize1x3
+	case w == 2 && h == 1:
+		return BuildingSize2x1
+	case w == 2 && h == 2:
+		return BuildingSize2x2
+	case w == 2 && h == 3:
+		return BuildingSize2x3
+	case w == 3 && h == 3:
+		return BuildingSize3x3
+	case w == 4 && h == 3:
+		return BuildingSize4x3
+	case w == 4 && h == 4:
+		return BuildingSize4x4
+	case w == 5 && h == 5:
+		return BuildingSize5x5
+	case w == 6 && h == 6:
+		return BuildingSize6x6
+	case w == 6 && h == 4:
+		return BuildingSize6x4
+	case w == 5 && h == 7:
+		return BuildingSize5x7
+	}
+
+	return BuildingSizeUnknown
+}
+
+// Building enthält die Informationen eines Gebäudes
+type Building struct {
+	BaseIndexSaved       int
+	BaseIndex            int
+	Rotation             int
+	X, Y                 int
+	AnimationSteps       int
+	CurrentAnimationStep int
+	AnimationAdd         int
+	Size                 BuildingSizeIdentifier
+}
