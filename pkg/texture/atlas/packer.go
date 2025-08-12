c6a1aec2ef636c845167933ad52fb16588bc74ce
Armin Schlegel <armin.schlegel@gmx.de>
2025-08-12 22:23:45 +0200

    fix: enhance atlas

--- pkg/texture/atlas/packer.go
+++ pkg/texture/atlas/packer.go
@@ -5,42 +5,70 @@ import (
 	"math"
 )
 
-func (p *MaxRectsPacker) findFreeRect(rectWidth, rectHeight int) (Rect, int, error) {
+func (p *MaxRectsPacker) findFreeRect(rectWidth, rectHeight int) (Rect, int, bool, error) {
 	bestRectIndex := -1
 	bestScore := math.MaxInt
+	bestRotated := false
 	for i, freeRect := range p.FreeRects {
+		// Try without rotation
 		if freeRect.Width >= rectWidth && freeRect.Height >= rectHeight {
 			score := p.scoreRect(freeRect, rectWidth, rectHeight)
 			if score < bestScore {
 				bestScore = score
 				bestRectIndex = i
+				bestRotated = false
+			}
+		}
+		// Try with rotation
+		if freeRect.Width >= rectHeight && freeRect.Height >= rectWidth {
+			score := p.scoreRect(freeRect, rectHeight, rectWidth)
+			if score < bestScore {
+				bestScore = score
+				bestRectIndex = i
+				bestRotated = true
 			}
 		}
 	}
 
 	if bestRectIndex == -1 {
-		return Rect{}, 0, fmt.Errorf("unable to pack rectangle of size %dx%d", rectWidth, rectHeight)
+		return Rect{}, 0, false, fmt.Errorf("unable to pack rectangle of size %dx%d", rectWidth, rectHeight)
 	}
-	return p.FreeRects[bestRectIndex], bestRectIndex, nil
+	return p.FreeRects[bestRectIndex], bestRectIndex, bestRotated, nil
 }
 
 // Pack tries to pack the given rectangle into the texture atlas
-func (p *MaxRectsPacker) Pack(rectWidth, rectHeight int) (Rect, error) {
-	// Find the smallest free rectangle that can fit the image
-	bestRect, index, err := p.findFreeRect(rectWidth, rectHeight)
+func (p *MaxRectsPacker) Pack(rectWidth, rectHeight int) (Rect, bool, error) {
+	bestRect, index, rotated, err := p.findFreeRect(rectWidth, rectHeight)
 	if err != nil {
-		return Rect{}, err
+		return Rect{}, false, err
 	}
-
-	// Split the free rectangle into two new rectangles (remaining free space)
-	p.splitFreeRect(index, rectWidth, rectHeight)
-
-	return Rect{X: bestRect.X, Y: bestRect.Y, Width: rectWidth, Height: rectHeight}, nil
+	w, h := rectWidth, rectHeight
+	if rotated {
+		w, h = rectHeight, rectWidth
+	}
+	p.splitFreeRect(index, w, h)
+	return Rect{X: bestRect.X, Y: bestRect.Y, Width: w, Height: h}, rotated, nil
 }
 
-// scoreRect calculates a score for the given placement of a rectangle
+// scoreRect calculates a score for the given placement of a rectangle (Best Area Fit)
 func (p *MaxRectsPacker) scoreRect(freeRect Rect, rectWidth, rectHeight int) int {
-	return int(math.Max(float64(freeRect.Width-rectWidth), float64(freeRect.Height-rectHeight)))
+	areaFit := (freeRect.Width * freeRect.Height) - (rectWidth * rectHeight)
+	shortSideFit := min(abs(freeRect.Width-rectWidth), abs(freeRect.Height-rectHeight))
+	return areaFit*1000 + shortSideFit // prioritize area fit, then short side fit
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // splitFreeRect splits a free rectangle into two new rectangles
@@ -58,9 +86,7 @@ func (p *MaxRectsPacker) splitFreeRect(index, rectWidth, rectHeight int) {
 			Width:  freeRect.Width - rectWidth,
 			Height: rectHeight,
 		}
-		// if newRect.Width >= 64 && newRect.Height >= 31 {
 		p.FreeRects = append(p.FreeRects, newRect)
-		// }
 	}
 
 	// Split the free rectangle vertically if there's enough space
@@ -71,9 +97,7 @@ func (p *MaxRectsPacker) splitFreeRect(index, rectWidth, rectHeight int) {
 			Width:  freeRect.Width,
 			Height: freeRect.Height - rectHeight,
 		}
-		// if newRect.Width >= 64 && newRect.Height >= 31 {
 		p.FreeRects = append(p.FreeRects, newRect)
-		// }
 	}
 }
 
---
+++ pkg/texture/atlas/packer_test.go
@@ -0,0 +1,77 @@
+package atlas
+
+import (
+	"testing"
+)
+
+func TestNewMaxRectsPacker(t *testing.T) {
+	width, height := 1024, 1024
+	packer := NewMaxRectsPacker(width, height)
+
+	if packer.Width != width || packer.Height != height {
+		t.Errorf("Expected packer dimensions to be %dx%d, got %dx%d", width, height, packer.Width, packer.Height)
+	}
+
+	if len(packer.FreeRects) != 1 {
+		t.Errorf("Expected 1 free rectangle, got %d", len(packer.FreeRects))
+	}
+
+	freeRect := packer.FreeRects[0]
+	if freeRect.Width != width || freeRect.Height != height {
+		t.Errorf("Expected free rectangle dimensions to be %dx%d, got %dx%d", width, height, freeRect.Width, freeRect.Height)
+	}
+}
+
+func TestPack(t *testing.T) {
+	packer := NewMaxRectsPacker(1024, 1024)
+
+	rectWidth, rectHeight := 512, 512
+	rect, rotated, err := packer.Pack(rectWidth, rectHeight)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	if rotated {
+		t.Errorf("Expected rectangle not to be rotated")
+	}
+
+	if rect.Width != rectWidth || rect.Height != rectHeight {
+		t.Errorf("Expected rectangle dimensions to be %dx%d, got %dx%d", rectWidth, rectHeight, rect.Width, rect.Height)
+	}
+
+	if rect.X != 0 || rect.Y != 0 {
+		t.Errorf("Expected rectangle position to be (0, 0), got (%d, %d)", rect.X, rect.Y)
+	}
+}
+
+func TestPackWithRotation(t *testing.T) {
+	packer := NewMaxRectsPacker(1024, 1024)
+
+	rectWidth, rectHeight := 512, 1024
+	rect, rotated, err := packer.Pack(rectWidth, rectHeight)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	if !rotated {
+		t.Errorf("Expected rectangle to be rotated")
+	}
+
+	if rect.Width != rectHeight || rect.Height != rectWidth {
+		t.Errorf("Expected rectangle dimensions to be %dx%d, got %dx%d", rectHeight, rectWidth, rect.Width, rect.Height)
+	}
+
+	if rect.X != 0 || rect.Y != 0 {
+		t.Errorf("Expected rectangle position to be (0, 0), got (%d, %d)", rect.X, rect.Y)
+	}
+}
+
+func TestPackFailure(t *testing.T) {
+	packer := NewMaxRectsPacker(512, 512)
+
+	rectWidth, rectHeight := 1024, 1024
+	_, _, err := packer.Pack(rectWidth, rectHeight)
+	if err == nil {
+		t.Fatalf("Expected error when packing rectangle larger than atlas")
+	}
+}
