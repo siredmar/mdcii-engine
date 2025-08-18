0ddae5acfd036c4fadabfd6c8b56c7a1ad8081b5
Armin Schlegel <armin.schlegel@gmx.de>
2025-08-18 23:05:11 +0200

    fix(building_from_atlas): port to raylib

--- cmd/building_from_atlas/cmd/root.go
+++ cmd/building_from_atlas/cmd/root.go
@@ -18,14 +18,13 @@ package cmd
 import (
 	"fmt"
 	"image"
-	"image/color"
-	"log"
 	"os"
 	"path/filepath"
 	"time"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/text"
+	rl "github.com/gen2brain/raylib-go/raylib"
+	r3d "github.com/siredmar/mdcii-engine/pkg/renderer/raylib"
+
 	"github.com/siredmar/mdcii-engine/pkg/bsh"
 	"github.com/siredmar/mdcii-engine/pkg/building"
 	"github.com/siredmar/mdcii-engine/pkg/cod"
@@ -35,35 +34,25 @@ import (
 	atlas "github.com/siredmar/mdcii-engine/pkg/texture/atlas"
 	"github.com/siredmar/mdcii-engine/pkg/world/rotation"
 	"github.com/spf13/cobra"
-	"golang.org/x/image/font/basicfont"
 )
 
 var (
 	gamePath      string
 	buildingIndex int
 	atlasPath     string
-	// buildingParam int
-
 )
 
 var (
-	ScreenWidth  int  = 600
-	ScreenHeight int  = 600
-	TileSize     int  = 64
-	GridEnable   bool = false
+	ScreenWidth  int = 600
+	ScreenHeight int = 600
+	TileSize     int = 64
 )
 
 func init() {
 	rootCmd.Flags().StringVarP(&atlasPath, "atlas", "a", ".", "Path to texture atlas")
 	rootCmd.Flags().StringVarP(&gamePath, "path", "p", ".", "Path to game")
 	rootCmd.Flags().IntVarP(&buildingIndex, "buildingIndex", "i", 381, "building index")
-	// rootCmd.Flags().IntVarP(&buildingParam, "building", "b", 2121, "building ID")
 }
-
-const (
-	tileWidth  = 64 // Breite eines Tiles in Pixel
-	tileHeight = 31 // Höhe eines Tiles in Pixel
-)
 
 var rootCmd = &cobra.Command{
 	Use:   "gfx viewer for buildings",
@@ -88,8 +77,7 @@ var rootCmd = &cobra.Command{
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		err = haeuserCod.Parse()
-		if err != nil {
+		if err = haeuserCod.Parse(); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -110,6 +98,7 @@ var rootCmd = &cobra.Command{
 			fmt.Println(err)
 			os.Exit(1)
 		}
+
 		var a *atlas.TextureAtlas
 		name := "texture-atlas"
 		a, err = atlas.LoadAtlasFromJSON(fmt.Sprintf("%s/%s.json", atlasPath, name))
@@ -125,39 +114,45 @@ var rootCmd = &cobra.Command{
 				return
 			}
 		}
-		// if buildingIndex == -1 {
-		// 	for i, b := range buildings.BuildingsVector {
-		// 		if b.Id == buildingParam {
-		// 			buildingIndex = i
-		// 			break
-		// 		}
-		// 	}
-		// }
 
-		ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
-		ebiten.SetWindowTitle("building_from_atlas")
+		rl.InitWindow(int32(ScreenWidth), int32(ScreenHeight), "building_from_atlas")
+		defer rl.CloseWindow()
+		rl.SetTargetFPS(60)
+
+		// Load atlas images into raylib textures
+		textures := make([]rl.Texture2D, len(a.Images))
+		for i, img := range a.Images {
+			rlImg := rl.NewImageFromImage(img)
+			textures[i] = rl.LoadTextureFromImage(rlImg)
+			rl.UnloadImage(rlImg)
+			defer rl.UnloadTexture(textures[i])
+		}
+
+		renderer := r3d.NewRenderer(float32(TileSize))
+
 		game := &Game{
 			windowWidth:    ScreenWidth,
 			windowHeight:   ScreenHeight,
 			tileSize:       TileSize,
 			gfxStadtfldBsh: gfxStadtfldBsh,
 			buildings:      buildings,
-			op:             &ebiten.DrawImageOptions{},
-			buffer:         ebiten.NewImage(ScreenWidth, ScreenHeight),
-			drawToBuffer:   true,
 			Building: &building.Building{
 				AnimationSteps:       0,
 				CurrentAnimationStep: 0,
 			},
 			buildingIndex:  buildingIndex,
 			atlas:          a,
-			Sprite:         nil,
 			rotation:       rotation.DEG0,
 			animationIndex: 0,
+			renderer:       renderer,
+			textures:       textures,
 		}
+
 		game.setBuilding(game.buildingIndex, 0)
-		if err := ebiten.RunGame(game); err != nil {
-			log.Fatal(err)
+
+		for !rl.WindowShouldClose() {
+			game.Update()
+			game.Draw()
 		}
 	},
 }
@@ -175,117 +170,90 @@ func (g *Game) setBuilding(index int, animationStep int) {
 	fmt.Println("Rotation", g.rotation)
 	fmt.Println("Animation Index", animationStep)
 	b := g.buildings.BuildingsVector[index]
-	sprite := g.atlas.ImagesMeta[b.Id].Animations[g.rotation].Images[animationStep].Sprite
+	spriteMeta := g.atlas.ImagesMeta[b.Id].Animations[g.rotation].Images[animationStep].Metadata
 
-	g.Building.Sprite = sprite
+	g.Building.Sprite = g.atlas.ImagesMeta[b.Id].Animations[g.rotation].Images[animationStep].Sprite
 	g.Building.AnimationSteps = b.AnimationAmount
 	g.Building.CurrentAnimationStep = animationStep
 	g.Building.Size = building.BuildingSize(b.Size.W, b.Size.H)
+
+	// Set texture and source rectangle for renderer
+	g.renderer.SetAtlasTexture(g.textures[spriteMeta.PNGIndex])
+	g.src = rl.NewRectangle(float32(spriteMeta.X), float32(spriteMeta.Y), float32(spriteMeta.Width), float32(spriteMeta.Height))
 }
 
 type Game struct {
-	windowWidth      int
-	windowHeight     int
-	tileSize         int
-	gfxStadtfldBsh   *bsh.BshPng
-	buildings        *buildings.Buildings
-	ScreenWidth      int
-	ScreenHeight     int
-	buffer           *ebiten.Image
-	op               *ebiten.DrawImageOptions
-	drawToBuffer     bool
-	Building         *building.Building
-	lastKeyPressTime time.Time
-	buildingIndex    int
-	atlas            *atlas.TextureAtlas
-	Sprite           image.Image
-	rotation         rotation.Rotation
-	animationIndex   int
+	windowWidth    int
+	windowHeight   int
+	tileSize       int
+	gfxStadtfldBsh *bsh.BshPng
+	buildings      *buildings.Buildings
+	Building       *building.Building
+	buildingIndex  int
+	atlas          *atlas.TextureAtlas
+	rotation       rotation.Rotation
+	animationIndex int
+	renderer       *r3d.Renderer
+	textures       []rl.Texture2D
+	src            rl.Rectangle
+	Sprite         image.Image
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (g *Game) Draw() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Black)
 
-	ebitenImg := ebiten.NewImageFromImage(g.Building.Sprite)
+	g.renderer.Begin()
+	// g.renderer.DrawGroundTile(0, 0)
+	g.renderer.DrawBillboardFromAtlas(g.src, g.src.Width, g.src.Height, 0, 0)
+	g.renderer.End()
 
-	options := &ebiten.DrawImageOptions{}
-	options.GeoM.Translate(100, 100)
-	// options.GeoM.Scale(1.5, 1.5)
-	screen.DrawImage(ebitenImg, options)
-	g.DrawBuildingInfo(screen)
-	g.DrawUsage(screen)
+	g.DrawBuildingInfo()
+	g.DrawUsage()
+
+	rl.EndDrawing()
 }
 
-func (g *Game) DrawBuildingInfo(screen *ebiten.Image) {
-	textColor := color.RGBA{255, 255, 255, 255}
-	face := basicfont.Face7x13
-
+func (g *Game) DrawBuildingInfo() {
 	b := g.buildings.BuildingsVector[g.buildingIndex]
-	text.Draw(screen, fmt.Sprintf("Building Id: %d", b.Id), face, 10, 10, textColor)
-	text.Draw(screen, fmt.Sprintf("Building Size: %d,%d", b.Size.W, b.Size.H), face, 10, 20, textColor)
-	text.Draw(screen, fmt.Sprintf("Building Animation Amount: %d", b.AnimationAmount), face, 10, 30, textColor)
-	text.Draw(screen, fmt.Sprintf("Building Animation Add: %d", b.AnimationAdd), face, 10, 40, textColor)
-	text.Draw(screen, fmt.Sprintf("Building Gfx: %d", b.Gfx), face, 10, 50, textColor)
-	text.Draw(screen, fmt.Sprintf("Building Rotation: %d", g.Building.Rotation), face, 10, 60, textColor)
-	text.Draw(screen, fmt.Sprintf("Building BaseIndex: %d", g.Building.BaseIndex), face, 10, 70, textColor)
-	text.Draw(screen, fmt.Sprintf("CurrentAnimationStep: %d", g.Building.CurrentAnimationStep), face, 10, 80, textColor)
+	rl.DrawText(fmt.Sprintf("Building Id: %d", b.Id), 10, 10, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building Size: %d,%d", b.Size.W, b.Size.H), 10, 20, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building Animation Amount: %d", b.AnimationAmount), 10, 30, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building Animation Add: %d", b.AnimationAdd), 10, 40, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building Gfx: %d", b.Gfx), 10, 50, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building Rotation: %d", g.Building.Rotation), 10, 60, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("Building BaseIndex: %d", g.Building.BaseIndex), 10, 70, 10, rl.RayWhite)
+	rl.DrawText(fmt.Sprintf("CurrentAnimationStep: %d", g.Building.CurrentAnimationStep), 10, 80, 10, rl.RayWhite)
 }
 
-func (g *Game) DrawUsage(screen *ebiten.Image) {
-	textColor := color.RGBA{255, 255, 255, 255}
-	face := basicfont.Face7x13
-
-	text.Draw(screen, "Up: Animation Step, Left/Right: Rotate, N: next, M: previous", face, 10, ScreenHeight-20, textColor)
-
+func (g *Game) DrawUsage() {
+	rl.DrawText("Up: Animation Step, Left/Right: Rotate, N: next, M: previous", 10, int32(ScreenHeight-20), 10, rl.RayWhite)
 }
 
-func (g *Game) Update() error {
-	const debounceDuration = time.Millisecond * 100
-
-	now := time.Now()
-
-	if now.Sub(g.lastKeyPressTime) >= debounceDuration {
-		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-			// g.Building.Rotation = (g.Building.Rotation + 3) % 4 // Linksrotation
-			g.rotation.Decrement()
-			g.setBuilding(g.buildingIndex, g.animationIndex)
-			g.lastKeyPressTime = now
-		} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-			// g.Building.Rotation = (g.Building.Rotation + 1) % 4 // Rechtsrotation
-			g.rotation.Increment()
-			g.setBuilding(g.buildingIndex, g.animationIndex)
-			g.lastKeyPressTime = now
-		} else if ebiten.IsKeyPressed(ebiten.KeyUp) {
-			fmt.Println(g.Building.AnimationSteps)
-			if g.Building.AnimationSteps > 0 {
-				g.Building.CurrentAnimationStep = (g.Building.CurrentAnimationStep + 1) % g.Building.AnimationSteps
-				// add := g.Building.CurrentAnimationStep * g.Building.AnimationAdd
-				// g.Building.BaseIndex = g.Building.BaseIndexSaved + add
-				// g.Building.CurrentAnimationStep++
-				// g.Building.CurrentAnimationStep = g.Building.CurrentAnimationStep % g.Building.AnimationSteps
-				g.setBuilding(g.buildingIndex, g.Building.CurrentAnimationStep)
-			}
-			g.lastKeyPressTime = now
-		} else if ebiten.IsKeyPressed(ebiten.KeyN) {
-			fmt.Println(g.buildingIndex)
-			g.buildingIndex = (g.buildingIndex + 1) % len(g.buildings.BuildingsVector)
-			fmt.Println(g.buildingIndex)
-			g.setBuilding(g.buildingIndex, g.animationIndex)
-			g.lastKeyPressTime = now
-		} else if ebiten.IsKeyPressed(ebiten.KeyM) {
-			if g.buildingIndex > 0 {
-				g.buildingIndex--
-			} else {
-				g.buildingIndex = len(g.buildings.BuildingsVector) - 1
-			}
-			g.setBuilding(g.buildingIndex, g.animationIndex)
-			g.lastKeyPressTime = now
-		} else if ebiten.IsKeyPressed(ebiten.KeyEscape) {
-			os.Exit(0)
+func (g *Game) Update() {
+	time.Sleep(100 * time.Millisecond)
+	if rl.IsKeyDown(rl.KeyLeft) {
+		g.rotation.Decrement()
+		g.setBuilding(g.buildingIndex, g.animationIndex)
+	} else if rl.IsKeyDown(rl.KeyRight) {
+		g.rotation.Increment()
+		g.setBuilding(g.buildingIndex, g.animationIndex)
+	} else if rl.IsKeyDown(rl.KeyUp) {
+		if g.Building.AnimationSteps > 0 {
+			g.Building.CurrentAnimationStep = (g.Building.CurrentAnimationStep + 1) % g.Building.AnimationSteps
+			g.setBuilding(g.buildingIndex, g.Building.CurrentAnimationStep)
 		}
+	} else if rl.IsKeyDown(rl.KeyN) {
+		g.buildingIndex = (g.buildingIndex + 1) % len(g.buildings.BuildingsVector)
+		g.setBuilding(g.buildingIndex, g.animationIndex)
+	} else if rl.IsKeyDown(rl.KeyM) {
+		if g.buildingIndex > 0 {
+			g.buildingIndex--
+		} else {
+			g.buildingIndex = len(g.buildings.BuildingsVector) - 1
+		}
+		g.setBuilding(g.buildingIndex, g.animationIndex)
+	} else if rl.IsKeyDown(rl.KeyEscape) {
+		os.Exit(0)
 	}
-	return nil
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return ScreenWidth, ScreenHeight
 }
--- pkg/renderer/raylib/renderer.go
+++ pkg/renderer/raylib/renderer.go
@@ -1,57 +1,99 @@
 package raylibrenderer
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"math"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type Renderer struct {
 	Camera   rl.Camera3D
-	PPU      float32      // pixels per world unit (64 matches your 64x32 base width)
-	AtlasTex rl.Texture2D // your atlas texture; set via SetAtlasTexture
+	PPU      float32
+	AtlasTex rl.Texture2D
+}
+
+const (
+	isoYawDeg   = 45.0
+	isoPitchDeg = 35.264389682 // asin(tan(30°))
+)
+
+func (r *Renderer) SetTrueIsoCamera(ppu float32) {
+	if ppu > 0 {
+		r.PPU = ppu
+	}
+
+	// 1 world unit = 1 tile pixel / PPU
+	r.Camera.Projection = rl.CameraOrthographic
+	r.Camera.Target = rl.NewVector3(0, 0, 0)
+	r.Camera.Up = rl.NewVector3(0, 1, 0)
+
+	// Exact yaw/pitch -> camera position on a sphere around the origin.
+	yaw := rl.Deg2rad * isoYawDeg
+	pitch := rl.Deg2rad * isoPitchDeg
+	dist := float32(20) // any positive distance (no perspective in ortho)
+
+	dir := rl.NewVector3(
+		float32(math.Cos(pitch)*math.Sin(yaw)),
+		float32(math.Sin(pitch)),
+		float32(math.Cos(pitch)*math.Cos(yaw)),
+	)
+	r.Camera.Position = rl.NewVector3(
+		-dir.X*dist, -dir.Y*dist, -dir.Z*dist,
+	)
+
+	// Ortho height in WORLD units (not degrees):
+	r.Camera.Fovy = float32(rl.GetScreenHeight()) / r.PPU
+}
+
+func (r *Renderer) OnResize() { // call when window size or zoom changes
+	r.Camera.Fovy = float32(rl.GetScreenHeight()) / r.PPU
 }
 
 func NewRenderer(ppu float32) *Renderer {
 	if ppu <= 0 {
-		ppu = 64
+		ppu = 64 // 1 world unit = 64 px
 	}
-	return &Renderer{
-		Camera: rl.Camera3D{
-			Position:   rl.NewVector3(10, 10, 10),
-			Target:     rl.NewVector3(0, 0, 0),
-			Up:         rl.NewVector3(0, 1, 0),
-			Fovy:       45,
-			Projection: rl.CameraOrthographic,
-		},
+	r := &Renderer{
 		PPU: ppu,
 	}
+	r.SetTrueIsoCamera(ppu)
+	// fovy is WORLD HEIGHT, not degrees. Keep world-units == pixels/PPU.
+	// r.Camera.Fovy = float32(rl.GetScreenHeight()) / r.PPU
+	return r
 }
+
+// // Call this if the window size or zoom (PPU) changes.
+// func (r *Renderer) UpdateOrthoFovy() {
+// 	r.Camera.Fovy = float32(rl.GetScreenHeight()) / r.PPU
+// }
 
 func (r *Renderer) SetAtlasTexture(tex rl.Texture2D) {
 	r.AtlasTex = tex
+	rl.SetTextureFilter(r.AtlasTex, rl.FilterPoint) // pixel-perfect
+	rl.SetTextureWrap(r.AtlasTex, rl.WrapClamp)     // no wrap sampling
+}
+func (r *Renderer) Begin() {
+	rl.BeginMode3D(r.Camera)
+}
+func (r *Renderer) End() {
+	rl.EndMode3D()
 }
 
-func (r *Renderer) Begin() { rl.BeginMode3D(r.Camera) }
-func (r *Renderer) End()   { rl.EndMode3D() }
-
-func (r *Renderer) DrawGroundTile(tileX, tileZ int) {
-	x := float32(tileX) + 0.5
-	z := float32(tileZ) + 0.5
-	rl.DrawPlane(rl.NewVector3(x, 0, z), rl.NewVector2(1, 1), rl.DarkGray)
+func insetRect(src rl.Rectangle, px float32) rl.Rectangle {
+	return rl.NewRectangle(src.X+px, src.Y+px, src.Width-2*px, src.Height-2*px)
 }
 
-// Draw a billboard using a sub-rectangle from the atlas (pixels).
-// Bottom-center sits on (tileX+0.5, 0, tileZ+0.5).
 func (r *Renderer) DrawBillboardFromAtlas(src rl.Rectangle, wPx, hPx float32, tileX, tileZ int) {
 	if r.AtlasTex.ID <= 0 {
 		return
 	}
 
-	// Convert pixel size -> world units
+	// 0.5–1.0 px usually enough; increase if you still see fringes (esp. with mipmaps)
+	src = insetRect(src, 0.5)
+
 	worldW := wPx / r.PPU
 	worldH := hPx / r.PPU
-
-	pos := rl.NewVector3(float32(tileX)+0.5, 0, float32(tileZ)+0.5)
-	// DrawBillboardRec centers the quad on pos; lift by half height to make bottom touch ground.
-	pos.Y += worldH * 0.5
-
+	pos := rl.NewVector3(float32(tileX)+0.5, worldH*0.5, float32(tileZ)+0.5)
 	size := rl.NewVector2(worldW, worldH)
 	rl.DrawBillboardRec(r.Camera, r.AtlasTex, src, pos, size, rl.White)
 }
